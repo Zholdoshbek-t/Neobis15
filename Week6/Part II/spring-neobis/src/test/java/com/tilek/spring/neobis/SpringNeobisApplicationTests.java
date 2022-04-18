e6baea842e35e2a55edcc39baea9016b272691e3
Tilek <zholdoshbek_t@auca.kg>
2022-04-19 00:31:07 +0600

    week6 modified

--- Week6/Part II/spring-neobis/src/test/java/com/tilek/spring/neobis/SpringNeobisApplicationTests.java
+++ Week6/Part II/spring-neobis/src/test/java/com/tilek/spring/neobis/SpringNeobisApplicationTests.java
@@ -1,13 +1,13 @@
 package com.tilek.spring.neobis;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class SpringNeobisApplicationTests {
+@SpringBootApplication
+public class SpringNeobisApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNeobisApplication.class, args);
 	}
 
 }
